84ae43638b387ab22c6855b99f64372ea05581a9
bamz-west <fabamiseadeolu@gmail.com>
2022-04-07 09:24:32 +0100

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from "./Transactions.json"; // an abi is the contract application binary interface that contains all the info abt a smart contract and is the standard way to interact with contrats in the ethereum ecosystem
+
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xa08846825b9232B5528dfF65c5947981D1e105bc";
